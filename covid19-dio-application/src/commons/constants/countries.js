7e40acfef65fea0e6d0319b9773fe1e31d34dc21
Gabriellfsj <gabriellfsj@gmail.com>
2022-05-01 16:05:11 -0300

    covid 19 application

---
+++ covid19-dio-application/src/commons/constants/countries.js
@@ -0,0 +1,59 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import IndiaFlag from '../../assets/images/india.png'
+import MexicoFlag from '../../assets/images/mexico.png'
+import RussiaFlag from '../../assets/images/russia.png'
+import PeruFlag from '../../assets/images/peru.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'Índia',
+    value: 'india',
+    flag: IndiaFlag
+  },
+  {
+    label: 'México',
+    value: 'mexico',
+    flag: MexicoFlag
+  },
+  {
+    label: 'Rússia',
+    value: 'russia',
+    flag: RussiaFlag
+  },
+  {
+    label: 'Peru',
+    value: 'peru',
+    flag: PeruFlag
+  }
+]
+
+export default COUNTRIES
